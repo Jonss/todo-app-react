a9c906794270d0ae261dae971c944396c7285834
Joao Santana <joaosantana.ti@gmail.com>
2018-02-27 23:25:01 -0300

    Creating Pageheader component and using on about and todo component

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default class About extends Component {
     render() {
         return (
             <div>
-                <h1>Sobre</h1>
+                <PageHeader name="Sobre" small="Nós"></PageHeader>
+                <h2>Nossa História</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur temporibus unde soluta, hic asperiores saepe debitis molestiae sint doloremque dignissimos, eaque cumque atque, quasi quo ipsam labore explicabo. Dolorem, deserunt?</p>
+                <h2>Missão e Visão</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et maxime exercitationem, nemo dolore sed repellendus, rem doloremque labore magnam sint iste minus, dolorum iusto expedita? Impedit officiis nisi voluptate ad?</p>
+                <h2>Imprensa</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et maxime exercitationem, nemo dolore sed repellendus, rem doloremque labore magnam sint iste minus, dolorum iusto expedita? Impedit officiis nisi voluptate ad?</p>
             </div>
         )
     }
