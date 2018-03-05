2df7f8074b822cd44bb45e37b48e803e3fac3414
Joao Santana <joao.santana@caelum.com.br>
2018-03-05 08:48:33 -0300

    Creating grid and iconButton component

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default class TodoForm extends Component {
     render() {
         return(
+
             <div role="form" className="todoForm">
-                <div className="col-xs-12  col-sm-9 col-md-9">
+                <Grid cols='12 9 10'>
                     <input  id="description" placeholder="Adicione uma tarefa" className="form-control"></input>
-                </div>
-                <div className="col-xs-12  col-sm-3 col-md-2">
-                    <button className="btn btn-primary">
-                        <i className="fa fa-plus"></i>
-                    </button>
-                </div>
+                </Grid>
+
+                <Grid cols='12 3 2'>
+                    <IconButton style='primary' icon='plus'></IconButton>
+                </Grid>
             </div>
         )
     }
