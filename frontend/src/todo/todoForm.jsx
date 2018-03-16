40c25ccab1f26f453e7e3ccce9041262584f4c17
João Santana <joaosantana.ti@gmail.com>
2018-03-16 17:05:29 -0300

    Adding done and undone operation. Also adding keyUp events

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -3,18 +3,39 @@ import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
 export default class TodoForm extends Component {
+
+    constructor(props) {
+        super(props)
+        console.log("Props" , props)
+        this.keyHandler = this.keyHandler.bind(this)
+
+    }
+
+    keyHandler(e) {
+        if (e.key == 'Enter') {
+            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+        } else if (e.key == 'Escape') {
+             this.props.handleClear() 
+        }
+    }
+
     render() {
         return(
 
             <div role="form" className="todoForm">
                 <Grid cols='12 9 10'>
-                    <input  id="description" placeholder="Adicione uma tarefa"
+                    <input  id="description" 
+                        placeholder="Adicione uma tarefa"
+                        onKeyUp={this.keyHandler}
                         onChange={this.props.handleChange}
-                     value={this.props.description} className="form-control"></input>
+                        value={this.props.description}
+                         className="form-control"></input>
                 </Grid>
 
                 <Grid cols='12 3 2'>
                     <IconButton style='primary' icon='plus' onClick={this.props.handleAdd}></IconButton>
+                    <IconButton style='info' icon='search' onClick={this.props.handleSearch}></IconButton>
+                    <IconButton style='default' icon='close' onClick={this.props.handleClear}></IconButton>
                 </Grid>
             </div>
         )
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -7,9 +7,11 @@ export default class TodoList extends Component {
         const list = this.props.list || []
         return list.map(todo => (
                 <tr key={todo._id}>
-                    <td>{todo.description}</td>
+                    <td className={todo.done? 'markedAsDone': ''}>{todo.description}</td>
                     <td>
-                        <IconButton style="danger" icon="trash-o" onClick={() => this.props.handleRemove(todo)}/>
+                        <IconButton style="success" icon="check" hide={todo.done} onClick={() => this.props.handleMarkAsDone(todo)}/>
+                        <IconButton style="warning" icon="undo" hide={!todo.done} onClick={() => this.props.handleMarkAsPending(todo)}/>
+                        <IconButton style="danger" icon="trash-o" hide={!todo.done} onClick={() => this.props.handleRemove(todo)}/>
                     </td>
                 </tr>
             ))
@@ -22,7 +24,7 @@ export default class TodoList extends Component {
                 <thead>
                     <tr>
                         <th>Descrição</th>
-                        <th>Ações</th>
+                        <th className='tableActions'>Ações</th>
                     </tr>
                 </thead>
                 <tbody>
